3442e24a3f005bc1f178f7d96648deae52291f66
RichardMPS <richard@lewisohn.co.uk>
2026-01-11 11:49:34 +0000

    Quick save 2026-01-11 11:49:34

--- git-save.sh
+++ git-save.sh
@@ -2,5 +2,10 @@
 cd ~/soundproof-app
 git add .
 git commit -m "Quick save $(date '+%Y-%m-%d %H:%M:%S')"
+git push -u origin main
+#!/bin/bash
+cd ~/soundproof-app
+git add .
+git commit -m "Quick save $(date '+%Y-%m-%d %H:%M:%S')"
 git push
 
